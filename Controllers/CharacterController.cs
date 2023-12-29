eec9250eaeeda8c14b39d74e56c7b8a7f0eb435a
ephatech <ephunashamed@gmail.com>
2023-12-29 15:08:24 -0800

    Before Database Migration

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+
+         private readonly ICharacterService _characterService;
+
+         public CharacterController(ICharacterService characterService){
+            _characterService = characterService;
+         }
+
+         [HttpGet("GetAll")]
+         public async Task<ActionResult<ServiceResponse<List<GetCharacterDTO>>>> Get(){
+            return Ok(await _characterService.GetAllCharacters());
+         }
+
+        [HttpGet("{id}")]
+         public async Task<ActionResult<ServiceResponse<GetCharacterDTO>>> GetSingle(int id){
+            return Ok(await _characterService.GetCharacterById(id));
+         }
+
+         [HttpPost]
+         public async Task<ActionResult<ServiceResponse<List<GetCharacterDTO>>>> AddCharacter(AddCharacterDTO newCharacter){
+            return Ok(await _characterService.AddCharacter(newCharacter));
+         }
+
+         [HttpPut]
+         public async Task<ActionResult<ServiceResponse<GetCharacterDTO>>> updateCharacter(UpdateCharacterDTO updatedCharacter){
+            var response = await _characterService.UpdateCharacter(updatedCharacter);
+            if(response.Data is null){
+               return NotFound(response);
+               }
+            return Ok(response);
+         }
+
+         [HttpDelete("{id}")]
+         public async Task<ActionResult<ServiceResponse<GetCharacterDTO>>> DeleteSingle(int id){
+            var response = await _characterService.DeleteCharacter(id);
+            if(response.Data is null){
+               return NotFound(response);
+               }
+            return Ok(response);
+         }
+
+    }
+}
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,101 @@
+global using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Id = 1, Name = "Sam" },
+            new Character {Id = 2, Name = "Ephrem"}
+         };
+
+        private readonly IMapper _mapper; 
+
+        public CharacterService(IMapper mapper){
+            _mapper = mapper;
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter){
+             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+             var character = _mapper.Map<Character>(newCharacter);
+             character.Id = characters.Max(c => c.Id) + 1;
+             characters.Add(character);
+             serviceResponse.Data=characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+             return serviceResponse;
+         }
+
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters(){
+             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+             serviceResponse.Data=characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+             return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> DeleteCharacter(int id){
+             var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+             
+             try{
+
+             var character = characters.FirstOrDefault(c => c.Id == id);
+             if(character is null)
+             throw new Exception($"Character with ID '{id}' not found!");
+
+            characters.Remove(character);
+
+            serviceResponse.Data=characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+
+             }
+
+             catch(Exception ex){
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+             }
+
+             return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id){
+             var serviceResponse = new ServiceResponse<GetCharacterDTO>();
+             var character = characters.FirstOrDefault(c => c.Id == id);
+             serviceResponse.Data = _mapper.Map<GetCharacterDTO>(character);
+             return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO updatedCharacter){
+             
+             var serviceResponse = new ServiceResponse<GetCharacterDTO>();
+             
+             try{
+
+             var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+             if(character is null)
+             throw new Exception($"Character with ID '{updatedCharacter.Id}' not found!");
+
+             character.Name = updatedCharacter.Name;
+             character.HitPoints = updatedCharacter.HitPoints;
+             character.Strength = updatedCharacter.Strength;
+             character.Defense = updatedCharacter.Defense;
+             character.Intelligence = updatedCharacter.Intelligence;
+             character.Class = updatedCharacter.Class;
+
+             serviceResponse.Data = _mapper.Map<GetCharacterDTO>(character);
+             
+             }
+
+             catch(Exception ex){
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+             }
+
+             return serviceResponse;
+        }
+
+        
+    }
+}
